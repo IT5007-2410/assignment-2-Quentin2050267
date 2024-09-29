d1eea00f037060ebdf72a8177915dbd4ab0f1ded
Quentin2050267 <2315902845@qq.com>
2024-09-29 18:06:25 +0800

    delete traveller

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,37 @@ const initialTravellers = [
   {
     id: 1,
     name: 'Jack',
-    phone: 88885555,
+    phone: '88885555',
+    email: 'jack@example.com',
+    seatNumber: '12A',
+    trainNumber: 'AA123',
     bookingTime: new Date(),
   },
   {
     id: 2,
     name: 'Rose',
-    phone: 88884444,
+    phone: '88884444',
+    email: 'rose@example.com',
+    seatNumber: '12B',
+    trainNumber: 'AA123',
+    bookingTime: new Date(),
+  },
+  {
+    id: 3,
+    name: 'John',
+    phone: '88883333',
+    email: 'john@example.com',
+    seatNumber: '14C',
+    trainNumber: 'BA456',
+    bookingTime: new Date(),
+  },
+  {
+    id: 4,
+    name: 'Doe',
+    phone: '88882222',
+    email: 'doe@example.com',
+    seatNumber: '14D',
+    trainNumber: 'BA456',
     bookingTime: new Date(),
   },
 ];
@@ -27,6 +51,9 @@ function TravellerRow(props) {
       <td>{traveller.id}</td>
       <td>{traveller.name}</td>
       <td>{traveller.phone}</td>
+      <td>{traveller.email}</td>
+      <td>{traveller.seatNumber}</td>
+      <td>{traveller.trainNumber}</td>
       <td>{traveller.bookingTime.toDateString()}</td>
     </tr>
   );
@@ -52,6 +79,9 @@ function Display(props) {
           <th>ID</th>
           <th>Name</th>
           <th>Phone</th>
+          <th>Email</th>
+          <th>Seat Number</th>
+          <th>Train Number</th>
           <th>Booking Time</th>
         </tr>
       </thead>
@@ -81,14 +111,21 @@ class Add extends React.Component {
     this.props.bookTraveller({
       name: form.travellername.value,
       phone: form.travellerphone.value,
+      email: form.travelleremail.value,
+      seatNumber: form.travellerseatnumber.value,
+      trainNumber: form.travellertrainnumber.value,
       bookingTime: new Date(),
       id: this.state.nextId,
     });
     form.travellername.value = '';
     form.travellerphone.value = '';
+    form.travelleremail.value = '';
+    form.travellerseatnumber.value = '';
+    form.travellertrainnumber.value = '';
     // form.travellerbookingtime.value = '';
     // form.travellerid.value = '';
     this.setState({ nextId: this.state.nextId + 1 });
+    alert('Add Success!');
   }
 
   render() {
@@ -97,6 +134,9 @@ class Add extends React.Component {
         {/*Q4. Placeholder to enter passenger details. Below code is just an example.*/}
         <input type="text" name="travellername" placeholder="Name" />
         <input type="text" name="travellerphone" placeholder="Phone" />
+        <input type="text" name="travelleremail" placeholder="Email" />
+        <input type="text" name="travellerseatnumber" placeholder="Seat Number" />
+        <input type="text" name="travellertrainnumber" placeholder="Train Number" />
         {/* <input type="text" name="travellerbookingtime" placeholder="Booking Time" /> */}
         {/* <input type="text" name="travellerid" placeholder="ID" /> */}
         <button>Add</button>
@@ -112,20 +152,28 @@ class Add extends React.Component {
 
 
 class Delete extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleSubmit(e) {
     e.preventDefault();
     /*Q5. Fetch the passenger details from the deletion form and call deleteTraveller()*/
+    const form = document.forms.deleteTraveller;
+    const id = parseInt(form.travellerid.value, 10);
+    this.props.deleteTraveller({
+      id: id,
+    });
+    form.travellerid.value = '';
+    alert('Delete Success!');
   }
 
   render() {
     return (
       <form name="deleteTraveller" onSubmit={this.handleSubmit}>
         {/*Q5. Placeholder form to enter information on which passenger's ticket needs to be deleted. Below code is just an example.*/}
-        <input type="text" name="travellername" placeholder="Name" />
+        {/* <input type="text" name="travellername" placeholder="Name" /> */}
+        <input type="text" name="travellerid" placeholder="ID" />
         <button>Delete</button>
       </form>
     );
@@ -187,6 +235,7 @@ class TicketToRide extends React.Component {
 
   deleteTraveller(passenger) {
     /*Q5. Write code to delete a passenger from the traveller state variable.*/
+    this.setState({ travellers: this.state.travellers.filter(traveller => traveller.id !== passenger.id) });
   }
   render() {
     return (
@@ -211,7 +260,7 @@ class TicketToRide extends React.Component {
           {/*Q4. Code to call the component that adds a traveller.*/}
           {this.state.selector === 3 && <Add bookTraveller={this.bookTraveller} travellers={this.state.travellers}/>}
           {/*Q5. Code to call the component that deletes a traveller based on a given attribute.*/}
-          {this.state.selector === 4 && <Delete />}
+          {this.state.selector === 4 && <Delete deleteTraveller={this.deleteTraveller} />}
         </div>
       </div>
     );
